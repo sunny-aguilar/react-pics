4e8ee77a0797c45f4242ac30d519d68cde38670b
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-15 22:55:49 -0700

    react

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,9 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
