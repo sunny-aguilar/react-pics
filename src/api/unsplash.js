3db75dc12739f5346a52c3e9af054edf2379a1f5
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-16 16:17:33 -0700

    react

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
-export default axios.create({
   // create a customizable instance for a particular URL
+export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
     Authorization: "Client-ID 3URMeY4GVY4fzD7bRp7GSTO9qyACuFzNosZC6N-MvWo",
