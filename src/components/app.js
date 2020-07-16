f7dfc50689cd2ded503b78700187d03dce1bea26
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-15 21:10:57 -0700

    react

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 
 
 const App = () => {
-  return (<div><SearchBar /></div>);
+  return (<div className="ui container"><SearchBar /></div>);
 }
 
 export default App;
