dda98f4cff29501cf5116059d1de76e56822630c
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-15 21:51:04 -0700

    react

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,7 +9,7 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
