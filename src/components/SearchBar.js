4f7ff076497d97bd5544c1a1bdfd8f34c3818d00
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-15 21:26:47 -0700

    react

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -20,7 +20,11 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              onClick="{this.onInputClick}"
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
