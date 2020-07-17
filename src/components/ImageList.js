5648fd3c86b9d5887bdd41f5c13825ea6aa5d0e8
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-16 22:11:04 -0700

    react

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,14 @@
 import React from "react";
-import "./ImageList.css"
+import ImageCard from "./ImageCard";
+import "./ImageList.css";
 
 const ImageList = (props) => {
   console.log(props.images);
-  const images = props.images.map(({alt_description, id, urls}) => {
-    return <img key={id} src={urls.regular} alt={alt_description} />;
+
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
+
   return <div className="image-list">{images}</div>;
 };
 
