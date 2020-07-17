f019e63ef34f689851441c65dc7bf06d578a4b9b
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-16 20:28:13 -0700

    react

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -5,7 +5,7 @@ const ImageList = (props) => {
   const images = props.images.map(({alt_description, id, urls}) => {
     return <img key={id} src={urls.regular} alt={alt_description} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
