f29b4ab998f05c3bca79c1ee2baff9e0fa359e10
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-16 18:33:14 -0700

    react

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ImageList = (props) => {
   console.log(props.images);
-  const images = props.images.map((image) => {return <img src={images} />});
+  const images = props.images.map((image) => {return <img src={image.urls.regular} />});
 return <div>{images}</div>;
 }
 
