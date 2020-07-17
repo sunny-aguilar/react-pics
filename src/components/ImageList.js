2f6498d4debb7243918dff20f0262e477a0ba9d9
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-16 18:32:35 -0700

    react

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const ImageList = (props) => {
   console.log(props.images);
-  const images = props.images.map(image => {return <img src={images} />});
-  return images;
+  const images = props.images.map((image) => {return <img src={images} />});
+return <div>{images}</div>;
 }
 
 export default ImageList;
