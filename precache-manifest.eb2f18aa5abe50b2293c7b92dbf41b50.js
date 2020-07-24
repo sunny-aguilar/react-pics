e57ad15f3430faa3b360b10397e45254bc696c3d
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-24 07:38:14 -0700

    Updates

---
+++ precache-manifest.eb2f18aa5abe50b2293c7b92dbf41b50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc24bc1640f46c0a11394efba9f8301",
+    "url": "/react-video-app/index.html"
+  },
+  {
+    "revision": "447c81959e8bafd5e01f",
+    "url": "/react-video-app/static/css/main.b4d62bb4.chunk.css"
+  },
+  {
+    "revision": "e33d29e69669432efcf3",
+    "url": "/react-video-app/static/js/2.58488f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-video-app/static/js/2.58488f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447c81959e8bafd5e01f",
+    "url": "/react-video-app/static/js/main.b815add5.chunk.js"
+  },
+  {
+    "revision": "a78fc7c3ff2e0197f28e",
+    "url": "/react-video-app/static/js/runtime-main.83ef9dfe.js"
+  }
+]);
